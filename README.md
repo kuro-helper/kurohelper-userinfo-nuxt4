fce4b8d9327fa6a0ca2b92899e20f5f1f61406e2
SeaotterMS <peter910820@gmail.com>
2026-04-09 20:35:15 +0800

    chore: init nuxt4 project and add vuetify(use vuetify-nuxt-module)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kurohelper-userinfo-nuxt4
+
 kurohelper的使用者資料建檔操作網站，使用nuxt4
---
+++ app/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <NuxtRouteAnnouncer />
+    <v-main>
+      <v-container class="py-8">
+        <v-card class="mx-auto" max-width="560">
+          <v-card-title>Kurohelper User Info</v-card-title>
+          <v-card-text>
+            Vuetify is ready with Nuxt 4 via <code>vuetify-nuxt-module</code>.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" variant="flat">Test Button</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
